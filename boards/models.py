8ab656234e7ba94f1f3d1e47f066690e9e7f781e
Younes Darabi <younes.darabi@outlook.com>
2026-03-15 22:58:37 +0100

    add models

--- boards/models.py
+++ boards/models.py
@@ -1,11 +1,12 @@
-# from django.db import models
-# from django.contrib.auth.models import User
+from django.db import models
+from users.models import User
 
-# class Board(models.Model):
 
-#     title = models.CharField(max_length=255)
-#     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_boards')
-#     members = models.ManyToManyField(User, related_name='boards')
+class Board(models.Model):
 
-#     def __str__(self):
-#         return self.title
+    title = models.CharField(max_length=30)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_boards')
+    members = models.ManyToManyField(User, related_name='boards')
+
+    def __str__(self):
+        return self.title
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+from boards.models import Board
+from users.models import User
+
+
+class Task(models.Model):
+
+    class Status(models.TextChoices):
+        TODO = 'to-do', 'To Do'
+        IN_PROGRESS = 'in-progress', 'In Progress'
+        REVIEW = 'review', 'Review'
+        DONE = 'done', 'Done'
+
+    class Priority(models.TextChoices):
+        LOW = 'low', 'Low'
+        MEDIUM = 'medium', 'Medium'
+        HIGH = 'high', 'High'
+
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='tasks')
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.TODO)
+    priority = models.CharField(max_length=20, choices=Priority.choices, default=Priority.MEDIUM)
+    assignee = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='assigned_tasks')
+    reviewer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='reviewed_tasks')
+    due_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+    
+
+class Comment(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author.fullname} on {self.task.title}"
